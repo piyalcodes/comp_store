fa6cc73f301cc01a0c38cb82b04d6a518e8f7427
Piyal <tenamax@gmail.com>
2018-02-16 11:01:43 +0530

    adding new routes with test page

---
+++ src_new/app/cart/cart.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+ 
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent implements OnInit {
+
+  
+
+  constructor() { }
+  
+   ngOnInit() {
+		console.log("ddd");
+	  } 
+ 
+}
---
+++ src_new/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+ 
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  
+
+  constructor() { }
+  
+   ngOnInit() {
+		console.log("ddd");
+	  } 
+ 
+}
